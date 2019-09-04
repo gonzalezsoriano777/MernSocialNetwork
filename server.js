4420485a5f64577d69b88d36e6ef9f532c3376ae
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-04 14:20:57 -0500

    Changed the port for the server.

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 3000; // To run Heroku using the port you must use 'process.env.PORT'
+const port = process.env.PORT || 8080; // To run Heroku using the port you must use 'process.env.PORT'
 
 app.listen(port, () => console.log(`Server running on port ${port}`)) // Using the back tics runs the ES6 for JS and you can run the server and call the port using the variable syntax .. which outputs whatever the port is
 
